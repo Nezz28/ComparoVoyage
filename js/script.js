a0f479a6af8574abbabce94221e3d3e621adb93f
Nezz28 <enzo1728@outlook.fr>
2025-05-16 11:42:04 +0200

    création page login + nouveau fichier CSS

--- js/script.js
+++ js/script.js
@@ -1,127 +1,17 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(function(response){
+    console.log(response)
+    return response.json()
 
-// Exercice 1
-
-/*function addition(a , b){
-    const somme = a + b 
-    return somme
-}
-
-console.log(addition(5 , 3))
-
-*/
-// Exercice 2
-
-/*function convertirCelsius(tempCelsius, tempFahrenheit){
-    const calc = tempCelsius * 9/5 + 32;
-    return calc;
-}
- 
-console.log(convertirCelsius(14))
-
-// Exercice 3
-
-function calculerAireRectangle(largeur, hauteur){
-    const aire = hauteur * largeur;
-    return aire
-}
-
-console.log(calculerAireRectangle(15, 15))
-
-// Exercice 4
-
-let saisie = prompt("Quel est le symbole de votre opération")
-const num1 = parseFloat(prompt("Premier nombre"));
-const num2 = parseFloat(prompt("Deuxième nombre"));
-
-if (saisie === "+") {
-    const result = addition(num1, num2);
-    alert("Résultat: " + result);
-}
-
-prompt("Premier nombre")
-prompt("deuxième nombre")
-
-function addition(a, b) {
-    const result = a + b;
-    return result;
-}
+})
 
 
-
-function soustraction(a,b){
-
-}
-
-function multiplication(a,b){
-    
-}
-
-function soustraction(a,b){
-    
-}
-*/
-
-// Exercice 1
-
-/*const livre = {
-    titre : "Objet javascript",
-    auteur : "Enzo",
-    nbPages : "96",
-};
+.then(function(data){
+console.log(data)
 
 
-livre.titre;
-livre["auteur"];
-livre.nbPages
+const user = data [0]
 
-const pages = "100"
+console.log(user)
 
-console.log(pages)
-
-// Exercice 2
-
-/*let nomAnimal = prompt("Nom de l'animal");
-let especeAnimal = prompt("Son espèce");
-let ageAnimal = prompt("Son âge");
-
-const animal = {
-    nom: nomAnimal,
-    espece: especeAnimal,
-    age: ageAnimal,
-};
-
-console.log(animal);*/
-
-//Exercice 3
-
-/*const etudiants = [
-    { nom: "Alice", age: 20, ville: "Paris" },
-    { nom: "Bob", age: 22, ville: "Lyon" },
-    { nom: "Charlie", age: 25, ville: "Marseille" },
-];
-
-console.log(etudiants[1].nom);
-
-
-etudiants.push({ nom: "Warix", age: 16, ville: "Nice" });
-
-
-for (let i = 0; i < etudiants.length; i++) {
-    if (etudiants[i].age >= 22) {
-        console.log(etudiants[i]);
-    }
-}
-
-// Exercice 4
-
-let ville = prompt("Saisissez une ville");
-
-
-for (let i = 0; i < etudiants.length; i++) {
-    if (etudiants[i].ville === ville) {
-        console.log(etudiants[i].nom);
-    }
-}
-*/
-
-
+});
