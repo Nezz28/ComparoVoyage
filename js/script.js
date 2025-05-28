647e0d50283dc60b33f2dc8d791e61cc1656a097
Nezz28 <enzo1728@outlook.fr>
2025-05-28 08:46:19 +0200

    reglage footer page login

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ if (logBox) {
     const email = document.getElementById('email').value;
 
   // Vérifie la présence de "@" et "."
+
   if (!email.includes('@') || !email.includes('.')) {
+
     e.preventDefault(); // Empêche l'envoi du formulaire
+    
     alert("L'adresse email doit contenir un '@' et un '.' !");
-    // Optionnel : mettre en rouge le champ
-    document.getElementById('email').style.borderColor = 'red';
-    return false;
-    document.getElementById('email').style.borderColor = '#FFAF00';
+
   }
 });
 }
